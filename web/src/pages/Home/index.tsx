f9183abd0dcd181841299de0528a2b559e651b77
Cinthia Godoi <cinthiagodoi@hotmail.com>
2020-06-23 17:38:25 +0100

    improve responsivity

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,39 +1,34 @@
 import React from 'react';
 import logo from '../../assets/logo.jpg';
-import {Link} from 'react-router-dom';
+import {Link, NavLink} from 'react-router-dom';
 
 import './styles.css';
 
 const Home = () => {
   return (
-    <div id='home-page'>
-      <div className='content'>
-
-        <header>
-          <img src={logo} alt='BookCase'/>
-        </header>
-      
-        <main>
-          <h1>Your online BookCase!</h1>
-          <p>Keep all your reading organized!</p>
-
-          <div className='log-in'> 
-            <div className='email_fiel'>
-              <input type='text' placeholder='e-mail'/>
-            </div>
-
-            <div className='password_field'>
-              <input type='text' placeholder='Password'/>
-            </div>
-           
-           <button type='submit'>SIGN IN</button>
-           <button type='submit'>CREATE AN ACCOUNT</button>
+    <div className='login'>
+      <header>
+        <img src={logo} alt='BookCase'/>
+      </header>
+    
+      <main>
+        <h1>Your online BookCase!</h1>
+        <p>Keep all your reading organized!</p>
+        
+        <input type='text' placeholder='e-mail'/>
+        <input type='text' placeholder='Password'/>
           
-          </div>
-        </main>   
-      </div>
+        <div className='actions'>
+          <button type='submit'>SIGN IN</button>
+          <button type='submit'>CREATE AN ACCOUNT</button>
+        </div>
+      </main>  
+
+      <footer><a href="https://www.freepik.com/free-photos-vectors/school">School vector created by pch.vector - www.freepik.com</a></footer>
     </div>
+ 
   )
 }
+
 
 export default Home;
