b9497a674b34f2c15937ef5a10a9a19b0f61e50a
Cinthia Godoi <cinthiagodoi@hotmail.com>
2020-07-09 16:40:50 +0100

    display error message

--- web/src/pages/CreateLogin/index.tsx
+++ web/src/pages/CreateLogin/index.tsx
@@ -1,90 +1,75 @@
-import React, {useState, FormEvent, ChangeEvent} from 'react';
-import {Link, useHistory} from 'react-router-dom';
+import React from "react";
+import { useForm } from "react-hook-form";
 import {FiArrowLeft} from 'react-icons/fi'
-import api from '../../services/api'
-
+import { Link, useHistory } from "react-router-dom";
 import logo from '../../assets/logo.jpg';
+import api from '../../services/api'
 import './styles.css'
 
-const CreateLogin = () => {
 
+
+const CreateUser = () => {
+  const { register, handleSubmit, errors } = useForm();
   const history = useHistory();
-
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    password: '',
-  })
-
-
-  function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
-    const { name, value } = event.target;
-    setFormData({...formData, [name]: value });
-  }
-
-  async function handleSubmit(event: FormEvent) {
-    event.preventDefault();
   
-    await api.post('users', formData)
+  async function onSubmit(data: any) {
+    await api.post('users', data)
       .then((res => {
         alert('User created!')
         history.push('/')
         return res.data
-      }))
-       .catch((err => {
-         if(err.response) {
-           let teste = err.response.data
-            for(const [key, value] of Object.entries(teste)){
-              console.log(teste)
-            }
-         }
-       }))    
+      }));
   }
 
-  return (
-    <div className='createUser'>
+   return (
+    <div className="createUser">
       <header>
         <img src={logo} alt='BookCase'/>
       </header>
 
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
         <h1>Create Your Account</h1>
         <fieldset>
-          <div className="field">
-            <label htmlFor="name">Name</label>
-            <input 
-              type="text" 
-              name="name"
-              id="name"
-              onChange = {handleInputChange}
-            />
-          </div>
-
-          <div className="field">
-            <label htmlFor="email">Email</label>
-            <input 
-              
-              type="text" 
-              name="email"
-              id="email"
-              
-              onChange = {handleInputChange}
+          <div className='field'>
+            <label htmlFor="inputName">Name</label>
+              <input
+                type="name"
+                id="name"
+                name="name"
+                ref={register({ required: "Enter your name" })}
               />
-              
-         
-            
-          </div>
+              {errors.name && <p className="error">{errors.name.message}</p>}
+            </div>
 
-          <div className="field">
-            <label htmlFor="password">Password</label>
-            <input 
-              type="password" 
-              name="password"
-              id="password"
-              onChange = {handleInputChange}
-            />
-          </div>
-        </fieldset>
+            <div className='field'>
+              <label htmlFor="inputEmail">E-mail</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                ref={register({
+                  required: "Enter your e-mail",
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                    message: "Enter a valid e-mail address",
+                  },
+                })}
+              />
+              {errors.email && <p className="error">{errors.email.message}</p>}
+            </div>
+
+            <div className='field'>
+              <label htmlFor="inputPassword">Password</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                ref={register({ required: "Enter your password" })}
+              />
+              {errors.password && <p className="error">{errors.password.message}</p>}
+            </div>
+          </fieldset>
+
 
         <div className='actions'>
           <Link to='/'>
@@ -94,12 +79,11 @@ const CreateLogin = () => {
           <button type="submit">
             Create Account
           </button>
-          
         </div>
-        
+
       </form>
     </div>
-  )
-}  
+  );
+};
 
-export default CreateLogin;
+export default CreateUser;
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom'
 
 import Home from './pages/Home'
-import CreateLogin from './pages/CreateLogin'
+import CreateUser from './pages/CreateLogin'
  
 const Routes = () => {
   return (
     <BrowserRouter>
       < Route component={Home} path='/' exact />
-      < Route component={CreateLogin} path='/create-account' exact />
+      < Route component={CreateUser} path='/create-account' exact />
     </BrowserRouter>
   )
 }
